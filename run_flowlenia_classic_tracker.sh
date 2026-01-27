185f0bef19cd4d7c25dd19408c4ae15fab8c6b5f
Mikhail Akhtyrchenko <makhtyrche@constructor.university>
2026-01-27 16:18:43 +0000

    fix

--- run_flowlenia_classic_tracker.sh
+++ run_flowlenia_classic_tracker.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python3 run_flowlenia_classic_tracker.py --video try.mp4 --out_dir segmentation --resize 224x224
+python3 run_flowlenia_classic_tracker.py --video output.mp4 --out_dir segmentation
